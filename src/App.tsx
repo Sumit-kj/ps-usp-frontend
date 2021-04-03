e1635d1029cd25baa2690c1109fa067ad801faeb
Sumit Kumar <sumitkj7.5@gmail.com>
2021-04-03 14:23:20 +0530

    demo purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import {QuizHome} from './QuizHome';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [items, setItems] = React.useState(
+      [
+        {'full_name': 'apple', 'key':1 },
+        {'full_name': 'bapple', 'key':2 },
+        {'full_name': 'capple', 'key':3 },
+        {'full_name': 'dapple', 'key':4 },
+        {'full_name': 'eapple', 'key':5 }]
   );
+  const [state, setState] = React.useState({value:''});
+  return (
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/quiz">
+              <Quiz />
+            </Route>
+            <Route path="/result">
+              <Results />
+            </Route>
+            <Route path="/">
+              <QuizHome/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+  );
+}
+
+
+function Home(props: any) {
+    return (
+        <div className="App">
+            <div className="App-content">
+                <h2>Home</h2>
+            </div>
+        </div>
+    );
+}
+
+function Quiz() {
+    return <h2>This is the List Page for Issues</h2>;
+}
+
+function Results() {
+    return <h2>This is the Details Page for Issues</h2>;
 }
 
 export default App;
